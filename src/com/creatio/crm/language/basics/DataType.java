d2d40477d67bddd060810d6da9172f21875be4a4
mayuri369 <mayurimayu369@gmail.com>
2024-12-22 23:46:24 +0530

    Assigment1 -Java Data Types

---
+++ src/com/creatio/crm/language/basics/DataType.java
@@ -0,0 +1,52 @@
+package com.creatio.crm.language.basics;
+
+public class DataType {
+
+	   public static void main(String[] args) {
+         float cityTemp=25.5f;
+         //Temperature of a city in degrees Celsius: 25.5
+         boolean orderPlaced=true;
+         //Whether a customer has placed an order: true or false
+         int phoneNumber=123-456-7890;
+         //phone number: "123-456-7890"
+         float bankBalance=1000.50f;
+         //Amount of money in a customer's bank account: 1000.50
+         String emailAddress="john.doe@example.com";
+         //Person's email address: "john.doe@example.com"
+         float latitudelocation=37.7749f;
+         float longitudelocation=-122.4194f;
+         //Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+         boolean maritalStatus=false;
+         //Person's marital status: true or false
+        String occupation= "Software Engineer";
+         //Person's occupation: "Software Engineer"
+        String colour="Blue";
+         // Person's favourite colour: "Blue"
+        Short currentYear=2023;
+         //Current year: 2023
+        String followers="1,000,000";
+        //Number of followers on a social media platform: 1,000,000
+        float rating=7.5f;
+        //Rating of a movie: 7.5
+        char blood_type ='A';
+        //Person's blood type: 'A'
+        String bookTitle="To Kill a Mockingbird";
+        //Title of a book: "To Kill a Mockingbird"
+        short employeeCount=500;
+        //Number of employees in a company: 500
+        String eventTime="2:30PM";
+        //Time of an event: 2:30 PM
+        String countryName="United States";
+        //Name of a country: "United States"
+        String eyeColor="Brown";
+        //Person's eye color: "Brown"
+        String brithPlace="New York City";
+        //Person's birthplace: "New York City"
+        float distance=200.5f;
+        //Distance between two cities:200.5
+     
+        	
+         
+	}
+
+}
